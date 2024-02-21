478671cac126d1eefccdf23424acfcc8c91badab
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 09:42:48 -0600

    Update CannonBall.py

--- CannonBall.py
+++ CannonBall.py
@@ -1,23 +1,23 @@
-#Import Pygame Module
+# Import the Pygame module
 import pygame
 
 class CannonBall:
 
-    #Constructor
-    def __init__(self, _img, x, y):
-        self.image = _img
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect((x, y), (self.image.get_width(), self.image.get_height()))
+    # Constructor
+    def __init__(self, image, x, y):
+        self.image = image  # Image of the cannonball
+        self.x = x  # X-coordinate of the cannonball
+        self.y = y  # Y-coordinate of the cannonball
+        self.rect = pygame.Rect((x, y), self.image.get_size())  # Rectangular area of the cannonball image
 
-    #Move the CannonBall Up
-    def moveUp(self, _vel):
-        return self.y - _vel
+    # Move the CannonBall Up
+    def move_up(self, velocity):
+        return self.y - velocity  # Move the cannonball up by the given velocity
 
-    #Get the Position of the CannonBall
-    def getPosition(self):
-        return (self.x, self.y)
+    # Get the Position of the CannonBall
+    def get_position(self):
+        return (self.x, self.y)  # Return the current position as a tuple
 
-    #Get the Resolution of the CannonBall's Image
-    def getResolution(self):
-        return (self.image.get_width(), self.image.get_height())
+    # Get the Resolution of the CannonBall's Image
+    def get_resolution(self):
+        return self.image.get_size()  # Return the image resolution as a tuple
