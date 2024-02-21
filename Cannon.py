4aac1dbc39e87a29a23c1d4c68fe3c688a8c596a
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 09:42:13 -0600

    Update Cannon.py

--- Cannon.py
+++ Cannon.py
@@ -1,33 +1,35 @@
-#Import Pygame Module
+# Import the Pygame module
 import pygame
 
 class Cannon:
 
-    #Constructor
-    def __init__(self, _img, x, y):
-        self.image = _img
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect((x, y), (self.image.get_width(), self.image.get_height()))
+    # Constructor
+    def __init__(self, image, x, y):
+        self.image = image  # Image of the cannon
+        self.x = x  # X-coordinate of the cannon
+        self.y = y  # Y-coordinate of the cannon
+        self.rect = pygame.Rect((x, y), self.image.get_size())  # Rectangular area of the cannon image
 
-    #Move Cannon Right
-    def moveRight(self, win, _vel):
+    # Move Cannon to the Right
+    def move_right(self, win, velocity):
+        # Check if moving right will keep the cannon within the window's boundaries
         if self.rect.midright[0] < (win.get_width() - 14):
-            return self.x + _vel
+            return self.x + velocity  # Move right by the given velocity
         else:
-            return self.x
+            return self.x  # Stay at the current position
 
-    #Move Cannon Left
-    def moveLeft(self, win, _vel):
+    # Move Cannon to the Left
+    def move_left(self, win, velocity):
+        # Check if moving left will keep the cannon within the window's boundaries
         if self.rect.midleft[0] > ((win.get_width() - win.get_width()) + 10):
-            return self.x - _vel
+            return self.x - velocity  # Move left by the given velocity
         else:
-            return self.x
+            return self.x  # Stay at the current position
 
-    #Get the Position of the CannonBall
-    def getPosition(self):
-        return (self.x, self.y)
+    # Get the Position of the Cannon
+    def get_position(self):
+        return (self.x, self.y)  # Return the current position as a tuple
 
-    #Get the Resolution of the CannonBall's Image
-    def getResolution(self):
-        return (self.image.get_width(), self.image.get_height())
+    # Get the Resolution of the Cannon's Image
+    def get_resolution(self):
+        return self.image.get_size()  # Return the image resolution as a tuple
